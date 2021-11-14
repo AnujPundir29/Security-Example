06e4135ad2b22a1cc32738a0bf81f4a9304223c0
AnujPundir29 <pundiranuj28@gmail.com>
2021-11-15 01:21:31 +0530

    setting up security measrures like oauth

--- server.js
+++ server.js
@@ -6,17 +6,33 @@ const helmet = require('helmet');
 
 app.use(helmet());
 
-app.get('/message', (req, res) => {
+function checkLoggedIn(req, res, next) {
+    const isLoggedIn = true;
+    if (!isLoggedIn) {
+        res.status(401).json({
+            error: 'You are not loggedin',
+        });
+    }
+    next();
+}
+
+app.get('/auth/google',(req, res) => {});
+
+app.get('/auth/google/callback',(req, res) => {});
+
+app.get('auth/logout',(req, res) => {});    
+
+app.get('/message', checkLoggedIn, (req, res) => {
     res.send('Hello World');
 });
 
 app.get('/*', (req, res) => {
-    res.send('this is the homepage');
+    res.sendFile(__dirname + '/index.html');
 });
 
 https.createServer({
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem'),
 }, app).listen(3001, () => {
-    console.log('Server running on port 3000');
+    console.log('Server running on port 3001');
 });
